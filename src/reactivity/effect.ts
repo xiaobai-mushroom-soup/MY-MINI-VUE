d7d70ade904b56a73643f17460755e18245532e5
grz <1605537551@qq.com>
2023-03-11 00:22:50 +0800

    effect:should return runner when call effect

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -5,7 +5,7 @@ class ReactiveEffect {
   }
   run() {
     activeEffect = this;
-    this._fn();
+    return this._fn();
   }
 }
 
@@ -44,4 +44,6 @@ export function effect(fn: any) {
   const _effect = new ReactiveEffect(fn);
 
   _effect.run();
+
+  return _effect.run.bind(_effect);
 }
